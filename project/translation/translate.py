f77170bec6ee5f59b5374522da6f3e28b63b68e1
Filip Chmiel <242238@edu.p.lodz.pl>
2025-01-25 20:21:24 +0100

    Added MBART

--- project/translation/translate.py
+++ project/translation/translate.py
@@ -7,15 +7,18 @@ import torch
 import pickle
 import torch.nn as nn
 import torch.nn.functional as F
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+import time
+from nltk import tokenize #, download
 
 SOS_token = 0
 EOS_token = 1
 device = "cpu"
 MAX_LENGTH = 20
 
-encoder_fp = "/en-de_trained_model/EN-DE-Encoder"
-decoder_fp = "/en-de_trained_model/EN-DE-Decoder"
-lang_fp = "/en-de_trained_model/eng-ger.pkl"
+encoder_fp = "E:/PDP/PDP-mds/project/translation/en-de_trained_model/EN-DE-Encoder.pt"
+decoder_fp = "E:/PDP/PDP-mds/project/translation/en-de_trained_model/EN-DE-Decoder.pt"
+lang_fp = "E:/PDP/PDP-mds/project/translation/en-de_trained_model/eng-ger.pkl"
 
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size, dropout_p=0.1):
@@ -172,29 +175,75 @@ def tensorFromSentence(lang, sentence):
     indexes.append(EOS_token)
     return torch.tensor(indexes, dtype=torch.long, device=device).view(1, -1)
 
-def translate(encoder, decoder, input_lang, output_lang, sentence):
+def translate_custom(encoder, decoder, input_lang, output_lang, fp_in, fp_out):
+    i = 0
     with torch.no_grad():
-        input_tensor = tensorFromSentence(input_lang, sentence)
+        with open(fp_in, 'r') as f:
+            sentences = f.read()
+        output = []
+        sentences = tokenize.sent_tokenize(sentences)
 
-        encoder_outputs, encoder_hidden = encoder(input_tensor)
-        decoder_outputs, decoder_hidden, decoder_attn = decoder(encoder_outputs, encoder_hidden)
+        for sentence in sentences:
+            input_tensor = tensorFromSentence(input_lang, sentence)
 
-        _, topi = decoder_outputs.topk(1)
-        decoded_ids = topi.squeeze()
+            encoder_outputs, encoder_hidden = encoder(input_tensor)
+            decoder_outputs, _, _ = decoder(encoder_outputs, encoder_hidden)
 
-        decoded_words = []
-        for idx in decoded_ids:
-            if idx.item() == EOS_token:
-                decoded_words.append('<EOS>')
-                break
-            decoded_words.append(output_lang.index2word[idx.item()])
-    decoded_words.pop(-1)
+            _, topi = decoder_outputs.topk(1)
+            decoded_ids = topi.squeeze()
+
+            decoded_words = []
+            for idx in decoded_ids:
+                if idx.item() == EOS_token:
+                    decoded_words.append('<EOS>')
+                    break
+                decoded_words.append(output_lang.index2word[idx.item()])
+            decoded_words.pop(-1)
+            punct = decoded_words[-1]
+            with open(fp_out, "a", encoding="utf-8") as f:
+                decoded_words[0] = decoded_words[0].capitalize()
+                f.write(" ".join(decoded_words[:-1]) + punct + " ")
+                i += 1
+                if i % 3 == 0:
+                    f.write("\n")
     '''
     #Optional prints to display the results in command line
     print('input =', sentence)
     print('output =', ' '.join(decoded_words))
-    return decoded_words
     '''
 
-translate(encoder, decoder, input_lang, output_lang, "This is an example sentence!")
+#MBART implementation
+def translate(fp_in, fp_out, lang):
+    model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+    tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+
+    lang_dict = {"pl": "pl_PL", "de": "de_DE", "ru": "ru_RU"}
+    
+    if lang not in lang_dict:
+        return "Incorrect language selected"
+
+    with open(fp_in, 'r') as f:
+        sentences = f.read()
+
+    output = []
+    sentences = tokenize.sent_tokenize(sentences)
+    for sentence in sentences:
+        tokenizer.src_lang = "en_XX"
+        encoded_hi = tokenizer(sentence, return_tensors="pt")
+        generated_tokens = model.generate(
+            **encoded_hi,
+            forced_bos_token_id=tokenizer.lang_code_to_id[lang_dict[lang]]
+        )
+        output.append(tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0])
+
+    with open(fp_out, "w", encoding="utf-8") as f:
+        i = 0
+        for sentence in output:
+            f.write(sentence + " ")
+            i += 1
+            if i % 3 == 0:
+                f.write("\n")
+    
+
+#translate(encoder, decoder, input_lang, output_lang, "test_file.txt", "test_file_out.txt")
 
